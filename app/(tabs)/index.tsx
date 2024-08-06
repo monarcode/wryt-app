7e53671a48224c3303681da37c850c3b69ce3d47
monarcode <ugbahisioma@gmail.com>
2024-08-06 20:25:43 +0100

    🧹 chore: updated basic pages content

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,12 +2,11 @@ import { StyleSheet, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { SketchCanvas } from '~/components/SketchCanvas';
-import { Text } from '~/components/shared';
 
 export default function Home() {
   return (
     <>
-      <SafeAreaView edges={['top']} style={{ flex: 1 }}>
+      <SafeAreaView edges={['top']} style={{ flex: 1, paddingHorizontal: 18 }}>
         <View style={styles.container}>
           <SketchCanvas containerStyle={{ flex: 1 }} />
         </View>
@@ -20,5 +19,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 24,
+    backgroundColor: '#fff',
+    marginBottom: 16,
+    borderRadius: 24,
   },
 });
